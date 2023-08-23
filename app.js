f18fbffbbe9a055d0fe8930fd06d12ad6e6bf576
nvspavankalyanch <pavankalyannvs.chaluwadi@gmail.com>
2023-08-23 12:17:28 +0530

    Added UI Updates

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');  
+   const app = express();
+   const http = require('http').createServer(app);
+   const io = require('socket.io')(http);
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+   app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+   app.get('/', (req, res) => {
+     res.sendFile(__dirname + '/index.html');
+   });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+   io.on('connection', (socket) => {
+     console.log('A user connected');
 
-  socket.on('message', (message) => {
-    io.emit('message', message); // Broadcast the message to all connected clients
-  });
+     socket.on('chat message', (msg) => {
+       io.emit('chat message', msg);
+     });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
+     socket.on('disconnect', () => {
+       console.log('A user disconnected');
+     });
+   });
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+   http.listen(3000, () => {
+     console.log('Server is running on http://localhost:3000');
+   });
