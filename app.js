ebda416428f66c57b60ef82fb6ceddcdbb2aedcd
N V S Pavan Kalyan <pavankalyannvs.chaluwadi@gmail.com>
2023-08-24 16:28:51 +0530

    feat: Add Emoji Search and Replace
    Some of the words in the messages after receiving to the client  will be converted into an emoji . The words and their emoji's were:
    
    react: "⚛",
    woah: "😲",
    hey: "👋",
    lol: "😂",
    like: "🤍",
    congratulations: "🎉",

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
 const express = require('express');
-   const app = express();
-   const http = require('http').createServer(app);
-   const io = require('socket.io')(http);
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-   app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-   app.get('/', (req, res) => {
-     res.sendFile(__dirname + '/index.html');
-   });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-   io.on('connection', (socket) => {
-     console.log('A user connected');
+// Define a mapping of special words and their corresponding emojis
+const emojiMapping = {
+  'hey': '👋',
+  'smile': '😊',
+  'heart': '❤️',
+  'love': '❤️',
+  'react': '⚛️',
+  'woah': '😲',
+  'lol': '😂',
+  'like': '❤️',
+  'congratulations': '🎉',
+  // Add more mappings as needed
+};
 
-     socket.on('chat message', (msg) => {
-       io.emit('chat message', msg);
-     });
+const onlineUsers = [];
 
-     socket.on('disconnect', () => {
-       console.log('A user disconnected');
-     });
-   });
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-   http.listen(3000, () => {
-     console.log('Server is running on http://localhost:3000');
-   });
+  socket.on('user join', (username) => {
+    onlineUsers.push(username);
+    io.emit('user join', onlineUsers);
+    socket.emit('online users', onlineUsers);
+  });
+
+  socket.on('chat message', (msg) => {
+    // Replace special words with emojis
+    const words = msg.text.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (emojiMapping.hasOwnProperty(words[i].toLowerCase())) {
+        words[i] = emojiMapping[words[i].toLowerCase()];
+      }
+    }
+    msg.text = words.join(' ');
+
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    const index = onlineUsers.indexOf(socket.username);
+    if (index !== -1) {
+      onlineUsers.splice(index, 1);
+      io.emit('user leave', onlineUsers);
+    }
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
